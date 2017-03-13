8fb3f120c316830f2f26f5618e88167817d3ab63
orenl <A1bC$5ef!>
2017-03-13 11:32:18 +0200

    Expenses App update

--- app/shared/services/httpService.ts
+++ app/shared/services/httpService.ts
@@ -2,10 +2,7 @@ import {Injectable} from "@angular/core";
 import {IInvoice} from "../interfaces/IInvoice";
 let http = require('http');
 
-const BASE_URL = 'http://192.168.1.2:8080/';
-// const BASE_URL = 'http://192.168.20.36/ExpensesRest/';
-// const BASE_URL = 'http://expensesrest.orenwebtest.com/';
-
+const BASE_URL = 'http://192.168.10.155:8080/';
 
 @Injectable()
 export class HttpService {
@@ -13,11 +10,14 @@ export class HttpService {
     constructor() {
     }
 
+
     users(){
+        console.log('Call url =>', `${BASE_URL}users`);
         return http.getJSON(`${BASE_URL}users`);
     }
 
     upload(invoiceData: IInvoice) {
+        console.log('Call url =>', `${BASE_URL}upload`);
         return http.request({
             url: `${BASE_URL}upload`,
             method: "POST",
